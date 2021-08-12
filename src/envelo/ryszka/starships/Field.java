f9ddd6f4225ac6b70b4982c5100f9238e722e2c8
mryszka98 <m.ryszka98@gmail.com>
2021-08-12 15:48:56 +0200

    initial commit

---
+++ src/envelo/ryszka/starships/Field.java
@@ -0,0 +1,8 @@
+package envelo.ryszka.starships;
+
+public enum Field {
+    WATER,
+    SHIP,
+    DARK,
+    HIT
+}
---
+++ src/envelo/ryszka/starships/StarShipsApp.java
@@ -0,0 +1,8 @@
+package envelo.ryszka.starships;
+
+public class StarShipsApp {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
