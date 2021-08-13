c968964cb8b2835effd7006dfbc9c6948721ef87
mryszka98 <m.ryszka98@gmail.com>
2021-08-13 11:20:00 +0200

    feat: implement UI.view1 method

--- src/envelo/ryszka/starships/StarShipsApp.java
+++ src/envelo/ryszka/starships/StarShipsApp.java
@@ -1,15 +1,9 @@
 package envelo.ryszka.starships;
 
-import envelo.ryszka.starships.enums.Field;
-import envelo.ryszka.starships.map.Map;
-
 public class StarShipsApp {
 
     public static void main(String[] args) {
-        Map test = new Map(30, Field.WATER);
-        test.set(3,2,Field.SHIP);
-        test.set(3,6,Field.HIT);
-        test.set(5,2,Field.DARK);
-        test.show();
+        UI ui = new UI();
+        ui.view1_userNames();
     }
 }
--- src/envelo/ryszka/starships/UI.java
+++ src/envelo/ryszka/starships/UI.java
@@ -1,16 +1,32 @@
 package envelo.ryszka.starships;
 
+import java.util.Scanner;
+
 public class UI {
     User user1;
     User user2;
     int maxPoints;
+    private Scanner in;
+    int mapSize;
+    private String firstUserName;
+    private String secondUserName;
+
+    public UI() {
+        in = new Scanner(System.in);
+    }
 
     void start() {
 
     }
 
     void view1_userNames() {
-
+        System.out.print("Enter map size: ");
+        mapSize = in.nextInt();
+        in.nextLine();
+        System.out.print("Enter first userName: ");
+        firstUserName = in.nextLine();
+        System.out.print("Enter second userName: ");
+        secondUserName = in.nextLine();
     }
     void view2_randomFirstUser() {
 
