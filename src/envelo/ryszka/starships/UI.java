39603ec7c5e15e7cb1bae188bdbb57f485463662
mryszka98 <m.ryszka98@gmail.com>
2021-08-13 11:10:48 +0200

    feat: Create skeleton for UI class

---
+++ src/envelo/ryszka/starships/UI.java
@@ -0,0 +1,27 @@
+package envelo.ryszka.starships;
+
+public class UI {
+    User user1;
+    User user2;
+    int maxPoints;
+
+    void start() {
+
+    }
+
+    void view1_userNames() {
+
+    }
+    void view2_randomFirstUser() {
+
+    }
+    void view3_shipsPositioning() {
+
+    }
+    void view4_game() {
+
+    }
+    void view5_theEnd() {
+
+    }
+}
